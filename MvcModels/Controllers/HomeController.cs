3f3a12d388f1bf416bc34de2543f8ff3c06a6ccf
narek.avanesyan <narek.avanesyan@FC-PROG-16>
2018-09-13 14:55:42 +0400

    Model Binding Attributes

--- MvcModels/Controllers/HomeController.cs
+++ MvcModels/Controllers/HomeController.cs
@@ -13,7 +13,32 @@ namespace MvcModels.Controllers
     {
         private IRepository repository;
         public HomeController(IRepository repo) { repository = repo; }
-        public ViewResult Index(int id) => View(repository[id]);
+        public IActionResult Index([FromQuery] int? id)
+        {
+            Person person;
+            if (id.HasValue && (person = repository[id.Value]) != null)
+                 return View(person);            
+            else
+                 return NotFound();            
+        }
+        public ViewResult Create() => View(new Person());
+
+        [HttpPost]
+        public ViewResult Create(Person model) => View("Index", model);
+
+        public ViewResult DisplaySummary([Bind(nameof(AddressSummary.City), Prefix =nameof(Person.HomeAddress))] AddressSummary summary) => View(summary);
+
+        public ViewResult Names(IList<string> names) => View(names ?? new List<string>());
+
+        public ViewResult Address(IList<AddressSummary> addresses) => View(addresses ?? new List<AddressSummary>());
+
+        //public string Header([FromHeader(Name = "Accept-Language")]string accept) => $"Header: {accept}";
+        public ViewResult Header(HeaderModel model) => View(model);
+
+        public ViewResult Body() => View();
+
+        [HttpPost]
+        public Person Body([FromBody]Person model) => model;
     }
 }
 
---
+++ MvcModels/Models/AddressSummary.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcModels.Models
+{
+    //[Bind(nameof(City))]
+    public class AddressSummary
+    {      
+        public string City { get; set; }
+
+        //[BindNever]
+        public string Country { get; set; }
+    }
+}
